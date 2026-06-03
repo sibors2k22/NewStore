408ebe01540c7b1dddc0b4abe6597e12d5586c5c
sibors2k22 <sibors.dbot@gmail.com>
2026-06-03 02:01:07 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const path = require('path');
-// Importa la configuración de la base de datos para activar la conexión
 const pool = require('./db'); 
 
 const app = express();
 
-// Middlewares para procesar datos de formularios y JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir archivos estáticos (nuestra página web y panel)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal: Carga la tienda al entrar al link
+// --- RUTAS DE LA API ---
+// Conectamos el archivo de rutas que acabas de crear
+app.use('/api/productos', require('./routes/productos'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Configuración del puerto para Railway
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Servidor activo y corriendo en el puerto ${PORT}`);
 });
